bbe1b0559d8542d62a372facc3479d1b5f1eb855
shajon1211045 <zahidulshajon@gmail.com>
2024-10-11 17:35:21 +0600

    updated files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './docs/**/*.{html,js,css}', // Adjust according to your folder structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
